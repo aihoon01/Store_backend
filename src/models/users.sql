ae4b16183db5e751ed2012c9af8a72e10bb31392
aihoon01 <aihoonstephen@gmail.com>
2022-12-09 12:19:59 +0000

    Updated SQL table

--- src/models/users.sql
+++ src/models/users.sql
@@ -5,8 +5,9 @@ CREATE type role as enum (
 
 CREATE TABLE users(
     id BIGSERIAL PRIMARY KEY NOT NULL,
-    firstname VARCHAR(50) NOT NULL,
-    lastname VARCHAR(50) NOT NULL,
+    firstname VARCHAR(50),
+    lastname VARCHAR(50),
+    bname VARCHAR (100),
     email VARCHAR(100) UNIQUE NOT NULL,
     password VARCHAR(100) NOT NULL,
     role role NOT NULL
