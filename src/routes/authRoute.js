cdff1957075b6e8d5a4ec1115806788f257370c2
aihoon01 <aihoonstephen@gmail.com>
2022-12-18 19:33:13 +0000

    changed verify-email to POST, swagger doc, login func

--- src/routes/authRoute.js
+++ src/routes/authRoute.js
@@ -1,24 +1,26 @@
 const express = require("express");
-const { signUp, logout, resetPassword, reset, access, verifytoken, vCheck } = require("../controllers/authController");
+const { signUp, logout, resetPassword, reset, access, verifytoken, vCheck, login } = require("../controllers/authController");
 const { validateSignUp, inputValidation, validateReset, checkAuthenticated } = require("../middlewares/verify_m");
 const passport = require("passport");
-
 const authRouter = express.Router();
+
+/**
+ * 
+ */
 
 // signupRoute 
 authRouter.post('/register', validateSignUp, inputValidation, signUp);
-// authRouter.get('/auth/register', access);
-authRouter.get('/verify-email', verifytoken);
+authRouter.post('/verify-email', verifytoken);
 
 // loginRoute
-authRouter.post('/login', vCheck, passport.authenticate("local"), access);
+authRouter.post('/login', vCheck, passport.authenticate("local"), login);
 
 //logout
 authRouter.get('/logout', logout);
 
 //Password Reset
 authRouter.post('/reset', resetPassword);
-authRouter.get('/reset-password', access);
+// authRouter.get('/reset-password', access);
 authRouter.post('/reset-password', validateReset, inputValidation, reset);
 
 
