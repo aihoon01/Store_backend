274bca93c2062909ee19bae6bda5dc8155c9563c
aihoon01 <aihoonstephen@gmail.com>
2023-01-10 11:22:35 +0000

    refactored update profile function and reconfigured cors

--- server.js
+++ server.js
@@ -27,7 +27,10 @@ app.use(passport.initialize());
 app.use(passport.session());
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
-app.use(cors());
+app.use(cors({
+    origin: "https://amalistore.netlify.app",
+    credentials: true
+}));
 app.use(fileUpload())
 
 //API ROUTES
